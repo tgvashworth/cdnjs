c9936097dc8a99d47c9a06e5ebd8de2fc48faf99
Tom Ashworth <tom@phuu.net>
2013-05-04 22:43:25 +0100

    Timeout to 5000 to allow tests to pass.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var cdnjs = require('../cdn.js'),
 describe('packages', function () {
 
   it('should pull down all packages correctly', function (done) {
+    this.timeout(5000);
     cdnjs.packages(function (err, packages) {
       should.not.exist(err);
       packages.should.be.ok;
@@ -17,6 +18,7 @@ describe('packages', function () {
 describe('search', function () {
 
   it('should find a known set of packages', function (done) {
+    this.timeout(5000);
     cdnjs.search('require', function (err, result) {
       should.not.exist(err);
       result.should.be.ok;
@@ -26,11 +28,8 @@ describe('search', function () {
     });
   });
 
-});
-
-describe('search', function () {
-
   it('should find a known url', function (done) {
+    this.timeout(5000);
     cdnjs.url('require.js', function (err, result) {
       should.not.exist(err);
       result.should.match(/\/\/cdnjs.cloudflare.com\/ajax\/libs\/require.js\/(.*)\/require.min.js/);
