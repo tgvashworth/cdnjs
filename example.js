ff647bacf59ee79cefa36941dd39610efc40b694
Tom Ashworth <tom@phuu.net>
2013-05-06 13:14:58 +0100

    Add example file.

---
+++ example.js
@@ -0,0 +1,23 @@
+var cdnjs = require('./'),
+    util = require('util');
+
+cdnjs.search('angular', function (err, packages) {
+  console.log('search:', util.inspect(packages, {
+    depth: null,
+    colors: true
+  }));
+});
+
+cdnjs.url('angular.js', function (err, packages) {
+  console.log('url:', util.inspect(packages, {
+    depth: null,
+    colors: true
+  }));
+});
+
+cdnjs.url('angular.js@1.0.0', function (err, packages) {
+  console.log('versioned url:', util.inspect(packages, {
+    depth: null,
+    colors: true
+  }));
+});
